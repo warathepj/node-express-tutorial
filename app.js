a0260b460741dd917c44727291af58c67b80dd96
warathepj <80021220+warathepj@users.noreply.github.com>
2021-06-13 14:49:57 +0700

    app.js const currentOS

--- app.js
+++ app.js
@@ -8,4 +8,7 @@ console.log(user);
 // method returns the system uptime in seconds
 console.log(`The System Uptime is ${os.uptime()} seconds`);
 
-
+const currentOS = {
+    name:os.type(),
+    release:os.release(),
+}
