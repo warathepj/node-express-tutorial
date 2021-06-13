1f9e629b73724cf9966ce04a6eba40aa7a83870a
warathepj <80021220+warathepj@users.noreply.github.com>
2021-06-13 17:23:49 +0700

    1 03 53

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ console.log(user);
 console.log(`The System Uptime is ${os.uptime()} seconds`);
 
 const currentOS = {
-    name:os.type(),
-    release:os.release(),
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
 }
+console.log(currentOS);
