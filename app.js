acb31251c65d5d7641a9aaa8d0d66a2958162bdb
warathepj <80021220+warathepj@users.noreply.github.com>
2021-06-23 19:07:15 +0700

    1 26 09

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ readFile('./content/first.txt', 'utf8', (err, result) => {
     }
     const first = result;
     readFile('./content/second.txt', 'utf8', (err, result) => {
-        
+        if (err) {
+            console.log(err);
+            return
+        }
+        const second = result
+        writeFile(
+            '/content/result-sync.txt',
+            `Here is the result : ${first}, ${second}` 
+            ,(err, result) => {
+                if(err){
+                    console.log(err);
+                    return;
+                }
+                console.log(result);
+            })
     })
 })
