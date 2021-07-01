0c812f36670c845d3548034e236d71c2aebab755
warathepj <80021220+warathepj@users.noreply.github.com>
2021-07-01 16:50:15 +0700

    1 39 31

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
-const { readFile, writeFile } = require('fs');
+const http = require('http');
 
-readFile('./content/first.txt', 'utf8', (err, result) => {
-    if (err) {
-        console.log(err);
-        return
-    }
-    const first = result;
-    readFile('./content/second.txt', 'utf8', (err, result) => {
-        if (err) {
-            console.log(err);
-            return
-        }
-        const second = result
-        writeFile(
-            '/content/result-sync.txt',
-            `Here is the result : ${first}, ${second}` 
-            ,(err, result) => {
-                if(err){
-                    console.log(err);
-                    return;
-                }
-                console.log(result);
-            })
-    })
+const server = http.createServer((req, res) => {
+    res.write('Welcome to our homepage');
 })
+
+server.listen(5000)
