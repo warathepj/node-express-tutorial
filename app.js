c8aa71cd1954150f915f8b4dfc329a55baa3f91b
warathepj <80021220+warathepj@users.noreply.github.com>
2021-07-02 19:28:48 +0700

    1 43 33

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write('Welcome to our homepage');
+    if(req.url === '/') {
+        res.end('Welcome to our homepage');
+    }
+    if(req.url === '/about') {
+        res.end('Here is our short history')
+    }
 })
 
 server.listen(5000)
