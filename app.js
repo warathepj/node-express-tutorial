2236bfcb8e8a956ea9997c3e298793f8db50e02d
warathepj <80021220+warathepj@users.noreply.github.com>
2021-07-03 12:06:56 +0700

    1 45 37

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const server = http.createServer((req, res) => {
     if(req.url === '/about') {
         res.end('Here is our short history')
     }
+    res.end(`
+    <h1>Oops</h1>
+    <p>no page</p>
+    <a href="/">back home</a>
+    `)
 })
 
 server.listen(5000)
+console.log('port 5000');
